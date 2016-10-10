b1904c44858f28811c5b339af155aea2c58c22c0
pwatfe <filope@gmail.com>
2016-10-10 15:18:44 +0000

    Script spin

---
+++ 2spin_forever.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+# Motor izq 4
+# Motor der 5
+
+import time
+import maestro as m
+s= m.Controller()
+
+
+while True:
+    s.setTarget(4,0)
+    s.setTarget(5,0)
+    s.setTarget(4,-1)
+    s.setTarget(5,1)
+
+    time.sleep(3)
+
+    s.setTarget(4,0)
+    s.setTarget(5,0)
+    s.setTarget(4,1)
+    s.setTarget(5,-1)
+
+    time.sleep(3)
+
--- cw.py
+++ cw.py
@@ -11,4 +11,9 @@ s= m.Controller()
 s.setTarget(4,1)
 s.setTarget(5,-1)
 
+time.sleep(5)
+
+s.setTarget(4,0)
+s.setTarget(5,0)
+
 
