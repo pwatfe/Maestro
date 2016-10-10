35cb8a442af5f709e40dd7729ff64c8f6113e697
pwatfe <filope@gmail.com>
2016-10-10 15:58:55 +0000

    Scripts infinitos de movimiento alterno izquierda/derecha y alante/atras

--- 2spin_forever.py
+++ 2spin_forever.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
 
-
-# Motor izq 4
-# Motor der 5
-
 import time
 import maestro as m
 s= m.Controller()
 
+# Motor izq salida 4
+servo_izq=4
+# Motor der salida 5
+servo_der=5
 
 while True:
-    s.setTarget(4,0)
-    s.setTarget(5,0)
-    s.setTarget(4,-1)
-    s.setTarget(5,1)
+    s.setTarget(servo_izq,-1)
+    s.setTarget(servo_der,1)
 
     time.sleep(3)
 
-    s.setTarget(4,0)
-    s.setTarget(5,0)
-    s.setTarget(4,1)
-    s.setTarget(5,-1)
+    s.setTarget(servo_izq,0)
+    s.setTarget(servo_der,0)
+    
+    # Espera minima de 0.4 para que coja el cambio de giro
+    time.sleep(0.4)
 
+    s.setTarget(servo_izq,1)
+    s.setTarget(servo_der,-1)
+    
     time.sleep(3)
+
+    s.setTarget(servo_izq,0)
+    s.setTarget(servo_der,0)
+
+    time.sleep(0.4)
 
